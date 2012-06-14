4790202d82884d058cc19c3b5647e25b4fa37b62
radu <zduci@yahoo.com>
2012-06-14 22:20:20 +0100

    added news controller

--- app/controllers/news_controller.rb
+++ app/controllers/news_controller.rb
@@ -1,2 +1,5 @@
 class NewsController < ApplicationController
+  def index
+    @news = News.get_page(params[:page])
+  end
 end
--- spec/controllers/news_controller_spec.rb
+++ spec/controllers/news_controller_spec.rb
@@ -1,4 +1,11 @@
 require 'spec_helper'
 
 describe NewsController do
+  it 'assigns the requested news page' do 
+    news = stub
+    page_number = '23'
+    News.stub(:get_page).with(page_number) { [news] } 
+    get :index, :page => page_number
+    assigns(:news).should == [news]
+  end
 end
