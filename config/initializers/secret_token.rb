1c39498536076ec7b40239ac076644f35f089707
radu <zduci@yahoo.com>
2012-06-14 21:28:39 +0100

    Initialized project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+HaX0rNews::Application.config.secret_token = '0e72fa12b575e398a7bb9d23d03ad776c2f4238de8cd944339114e275020681742148c8ef78e1a63bd7ffa8884a80530e047ebeec9a7daa2752203f8bbd2d175'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+HaX0rNews::Application.config.session_store :cookie_store, key: '_haX0r_news_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# HaX0rNews::Application.config.session_store :active_record_store
