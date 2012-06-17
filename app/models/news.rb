e675ea0b2e027663111c4d916c05c6c16c246b38
radu <zduci@yahoo.com>
2012-06-17 14:01:57 +0100

    News model checks if page exists

--- app/models/news.rb
+++ app/models/news.rb
@@ -1,13 +1,15 @@
 class News < ActiveRecord::Base
-  def self.page_exists(page_number)
-    
+  DEFAULT_ARTICLES_ON_PAGE = 20
+
+  def self.page_exists(page, articles_on_page = DEFAULT_ARTICLES_ON_PAGE)
+    News.count > create_offset(page, articles_on_page)
   end
 
   def self.create_article(author, title, content)
     News.create!(:author => author, :title => title, :content => content)
   end
 
-  def self.get_page(page, articles_on_page = 20)
+  def self.get_page(page, articles_on_page = DEFAULT_ARTICLES_ON_PAGE)
     offset = create_offset(page, articles_on_page)
     News.order("created_at DESC").limit(articles_on_page).offset(offset)
   end
--- spec/models/news_spec.rb
+++ spec/models/news_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 describe News do
+  let(:articles_on_page) { 1 }
+    
   it 'retrieves the articles for the requested page' do
-    articles_on_page = 1
     first_article = News.create_article("Anonymous", "First article", "This should be on page 2")
     second_article = News.create_article("Anonymous", "Second article", "This should be on page 1")
     first_page = News.get_page(1, articles_on_page)
     second_page = News.get_page(2, articles_on_page)
     first_page.should == [second_article]
     second_page.should == [first_article]
+  end
+
+  it 'checks if a page exists' do
+    first_article = News.create_article("Anonymous", "single", "Forever alone")
+    News.page_exists(1, 1).should == true
+    News.page_exists(2, 1).should == false
   end
 end
