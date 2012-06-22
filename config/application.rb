c3e0bacb8661737decce05b2da93121c3f1e3fe8
radu <zduci@yahoo.com>
2012-06-22 22:05:56 +0100

    Installed devise

--- config/application.rb
+++ config/application.rb
@@ -55,5 +55,8 @@ module HaX0rNews
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # Heroku deployment
+    config.assets.initialize_on_precompile = false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 HaX0rNews::Application.routes.draw do
+  root :to => 'news#index'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
